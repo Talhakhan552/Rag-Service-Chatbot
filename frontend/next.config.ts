94888bd9355052fe0cdb664b759bfb09f81c4584
Talhakhan552 <talhakhan11331@gmail.com>
2026-08-22 12:07:08 +0500

    Prepare to deploy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: process.env.VERCEL ? undefined : "standalone",
+};
 
 export default nextConfig;
